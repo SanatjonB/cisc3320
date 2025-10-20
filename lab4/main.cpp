b0ce76c94fe03fcbaa3e36b7f06e41f688be760a
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2025-10-19 20:38:54 -0400

    lab4 done

---
+++ lab4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+#include <cstdlib>
+using namespace std;
+void signalHandler(int signalNum) {
+    cout <<"Interupt signal recieved exciting safely: "<< endl;
+    exit(0);
+}
+int main() {
+    signal(SIGINT, signalHandler);
+    cout << "Waiting for user intterupt (Ctlr + C ): " << endl;
+    while (true) {
+        //pause(); did not work on windows but i did see on stack overflow it works on everything else. such as mac linux and ubuntu/unix.
+    }
+    return 0;
+}
